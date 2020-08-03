8217662934d350c9ffed29b5f11100b7955c7987
Roman Plášil <me@rplasil.name>
2020-08-03 15:57:16 +0800

    Improve dedup script

--- tools/photodedup/scanhash.py
+++ tools/photodedup/scanhash.py
@@ -4,15 +4,30 @@ import hashlib
 
 from os.path import join
 
-
 start = sys.argv[1]
-logfile = open('hashes.lst', 'a')
+logpath = 'hashes.lst'
 
+def read_list(fname):
+    result = {}
+    if not os.path.exists(fname): return {}
+
+    with open(fname) as inf:
+        for line in inf:
+            parts = line.split('\t')
+            result[parts[0]] = parts[1].strip()
+
+    return result
+
+existing = read_list(logpath)
+existing_fnames = (existing.values())
+logfile = open(logpath + '.2', 'a')
 
 def report(h, path):
     logfile.write('{}\t{}\n'.format(h, path))
 
 def do_file(full):
+    if full in existing_fnames: return
+
     with open(full, 'rb') as inf:
         h = hashlib.sha1()
         while True:
@@ -21,6 +36,7 @@ def do_file(full):
             h.update(data)
 
         report(h.hexdigest(), full)
+
 
 for root, dirs, files in os.walk(start):
     for name in files:
